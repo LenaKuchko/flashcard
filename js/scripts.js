43d4ad151c5c52f1920fc7f4e744f1f23b3bc4d8
Andrew Glines and Olena Kuchko <andrew@>
2017-04-25 16:09:41 -0700

    add button(add img) which adds images

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,40 @@
 $(document).ready(function() {
 
-  $(".clickable#1").click(function() {
-    $("#java1").slideToggle();
+  $("li").click(function(){
+    $(this).next().slideToggle();
   });
-  $(".clickable#2").click(function() {
-    $("#oper2").slideToggle();
+
+  $("button#show").click(function(){
+    $("img").show("hidden");
+
+    // $("img").addClass("hidden");
+
   });
-  $(".clickable#3").click(function() {
-    $("#var3").slideToggle();
+
+  $("button#show").click(function(){
+    $("img").hide();
   });
-  $(".clickable#4").click(function() {
-    $("#met4").slideToggle();
+
+  $("button#add").click(function() {
+    $("body").append("<img src= \"img/download.jpeg\">");
   });
-  $(".clickable#5").click(function() {
-    $("#fun5").slideToggle();
-  });
+
+
+  // $(".clickable#1").click(function() {
+  //   $("#java1").slideToggle();
+  // });
+  // $(".clickable#2").click(function() {
+  //   $("#oper2").slideToggle();
+  // });
+  // $(".clickable#3").click(function() {
+  //   $("#var3").slideToggle();
+  // });
+  // $(".clickable#4").click(function() {
+  //   $("#met4").slideToggle();
+  // });
+  // $(".clickable#5").click(function() {
+  //   $("#fun5").slideToggle();
+  // });
 
   // var ids = ["java1", "oper2"];
   // var idP = ["java1", "oper2"];
@@ -30,25 +50,3 @@ $(document).ready(function() {
   //     }
   //   };
   });
-
-
-
-  //   if (str===idP[i]) {
-  //     $(str).click(function(){
-  //     $(idP[i]).slideToggle();
-  //   });
-  // });
-    // $(str).click(function() {
-    //   // console.log(str);
-    //   // console.log("Click Handler Fired!")
-    //   //var liselector = "#"+ids[i];
-    //   //var selector = "#"+idP[i];
-    //
-    //   //$(liselector).slideToggle();
-    //   // console.log("#"+idP[i]);
-    // });
-
-  //}
-
-
-//});
